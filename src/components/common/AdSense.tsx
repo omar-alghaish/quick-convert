1e8bac80491f6a8e5e56a0c471d1e1ee52ba22ce
omar alghaish <omaralghish@gmail.com>
2025-02-10 20:03:48 +0200

    some changes

--- src/components/common/AdSense.tsx
+++ src/components/common/AdSense.tsx
@@ -1,19 +1,19 @@
 import Script from "next/script";
-import React from "react";
 
 type AdSenseTypes = {
   pId: string;
 };
 
 const AdSense = ({ pId }: AdSenseTypes) => {
-    console.log(pId)
   return (
-    <Script
-      async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4613977372487688`}
-      crossOrigin="anonymous"
-      strategy="afterInteractive"
-    />
+    <>
+      <Script
+        async
+        strategy="beforeInteractive"
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${pId}`}
+        crossOrigin="anonymous"
+      />
+    </>
   );
 };
 
