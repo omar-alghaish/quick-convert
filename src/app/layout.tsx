9de94e9cdfe52fb73fc4d101ddd456cb80b4eb4a
omar alghaish <omaralghish@gmail.com>
2025-02-10 20:19:48 +0200

    some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/common/AppSidebar";
 import AdSense from "@/components/common/AdSense";
-// import Head from "next/head";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,10 +31,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
               <AdSense pId="ca-pub-4613977372487688" />
   
-      </head>
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <SidebarProvider>
           <AppSidebar />
